04c7cdbb26e94f4872a36a71518db85cc6465d9b
Lucas Volpato <lmaciel916@gmail.com>
2023-06-23 15:08:41 -0300

    :tada: first commit

--- src/index.js
+++ src/index.js
@@ -1,45 +1,105 @@
-const express = require('express');
-const { v4: uuidv4} = require('uuid')
+const express = require("express");
+const { v4: uuidv4 } = require("uuid");
 
 const app = express();
 app.use(express.json());
 
 const custumers = [];
 
+// verificando se a conta exsite
+function verifyIfAccountExistsCPF(req, res, next) {
+  const { cpf } = req.headers;
+
+  // validando se customer existe
+  const custumer = custumers.find((custumer) => custumer.cpf === cpf);
+
+  if (!custumer) {
+    return res.status(400).json({ error: "Customer not found!" });
+  }
+
+  req.custumer = custumer;
+
+  return next();
+}
+
+// fazendo o balanço
+function getBalance(statement) {
+  const balance = statement.reduce((acc, operation) => {
+    if (operation.type === "credit") {
+      return acc + operation.amount;
+    } else {
+      return acc - operation.amount;
+    }
+  }, 0);
+
+  return balance;
+}
+
 // criando a conta
 app.post("/account", (req, res) => {
-    const { cpf, name } = req.body
+  const { cpf, name } = req.body;
 
+  // validação de cpf
+  const costumerAlreadyExists = custumers.some(
+    (custumer) => custumer.cpf === cpf
+  );
+  if (costumerAlreadyExists) {
+    return res.status(400).json({ error: "Custumer already exists!" });
+  }
 
-    // validação de cpf
-    const costumerAlreadyExists = custumers.some((custumer) => custumer.cpf === cpf)
-    if(costumerAlreadyExists) {
-        return res.status(400).json({error: "Custumer already exists!"})
-    }
+  custumers.push({
+    cpf,
+    name,
+    id: uuidv4(),
+    statement: [],
+  });
 
-    custumers.push({
-        cpf,
-        name, 
-        id: uuidv4(), 
-        statement: []
-    })
-
-    return res.status(201).send()
+  return res.status(201).send();
 });
 
 // verificando statement
-app.get("/statement/:cpf", (req, res) => {
-    const { cpf } = req.params; 
+app.get("/statement", verifyIfAccountExistsCPF, (req, res) => {
+  const { custumer } = req;
+  return res.json(custumer.statement);
+});
 
-    const custumer = custumers.find((custumer) => custumer.cpf === cpf);
+// fazendo deposito
+app.post("/deposit", verifyIfAccountExistsCPF, (req, res) => {
+  const { description, amount } = req.body;
 
-    // validando se customer existe
-    if(!custumer) {
-        return res.status(400).json({error: "Customer not found!"})
-    }
+  const { custumer } = req;
 
-    return res.json(custumer.statement)
-})
+  const statementOperation = {
+    description,
+    amount,
+    createdAt: new Date(),
+    type: "credit",
+  };
 
-app.listen(3000)
+  custumer.statement.push(statementOperation);
 
+  return res.status(201).send();
+});
+
+// fazendo saque
+app.post("/withdraw", verifyIfAccountExistsCPF, (req, res) => {
+  const { amount } = req.body;
+  const { custumer } = req;
+
+  const balance = getBalance(custumer.statement);
+
+  if (balance < amount) {
+    return res.status(400).json({ error: "Insufficient founds!" });
+  }
+  const statementOperation = {
+    amount,
+    createdAt: new Date(),
+    type: "debit",
+  };
+
+  custumer.statement.push(statementOperation);
+
+  return res.status(201).send();
+});
+
+app.listen(3000);
